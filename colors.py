ea4ba41f90dac218e778d93224c419fdf76638e7
rajdip20 <dasrajdip78754282@gmail.com>
2020-10-18 15:24:27 +0530

    Initial commit

---
+++ colors.py
@@ -0,0 +1,64 @@
+SIZE = 400
+GRID_LEN = 4
+GRID_PADDING = 10
+
+BACKGROUND_COLOR_GAME = "#92877d"
+BACKGROUND_COLOR_CELL_EMPTY = "#9e948a"
+
+BACKGROUND_COLOR_DICT = {
+    2: "#ffff94",
+    4: "#1521c1",
+    8: "#00ccff",
+    16: "#f59563",
+    32: "#00ff99",
+    64: "#009900",
+    128: "#ff66a3",
+    256: "#cc0099",
+    512: "#ff4d4d",
+    1024: "#ff5500",
+    2048: "#edc22e",
+
+    4096: "#eee4da",
+    8192: "#edc22e",
+    16384: "#f2b179",
+    32768: "#f59563",
+    65536: "#f67c5f"
+}
+
+CELL_COLOR_DICT = {
+    2: "#776e65",
+    4: "#776e65",
+    8: "#f9f6f2",
+    16: "#f9f6f2",
+    32: "#f9f6f2",
+    64: "#f9f6f2",
+    128: "#f9f6f2",
+    256: "#f9f6f2",
+    512: "#f9f6f2",
+    1024: "#f9f6f2",
+    2048: "#f9f6f2",
+
+    4096: "#776e65",
+    8192: "#f9f6f2",
+    16384: "#776e65",
+    32768: "#776e65",
+    65536: "#f9f6f2"
+}
+
+FONT = ("Verdana", 40, "bold")
+
+KEY_UP_ALT = "\'\\uf700\'"
+KEY_DOWN_ALT = "\'\\uf701\'"
+KEY_LEFT_ALT = "\'\\uf702\'"
+KEY_RIGHT_ALT = "\'\\uf703\'"
+
+KEY_UP = "'8'"
+KEY_DOWN = "'2'"
+KEY_LEFT = "'4'"
+KEY_RIGHT = "'6'"
+KEY_BACK = "'r'"
+
+KEY_J = "'j'"
+KEY_K = "'k'"
+KEY_L = "'l'"
+KEY_H = "'h'"
---
+++ logic.py
@@ -0,0 +1,145 @@
+import random
+import colors as c
+
+def new_game(n):
+    matrix = []
+
+    for i in range(n):
+        matrix.append([0] * n)
+    return matrix
+
+def add_two(mat):
+    a = random.randint(0, len(mat)-1)
+    b = random.randint(0, len(mat)-1)
+    while mat[a][b] != 0:
+        a = random.randint(0, len(mat)-1)
+        b = random.randint(0, len(mat)-1)
+    mat[a][b] = 2
+    return mat
+
+def game_state(mat):
+
+    for i in range(len(mat)):
+        for j in range(len(mat[0])):
+            if mat[i][j] == 2048:
+                return 'win'
+
+    for i in range(len(mat)-1):
+        for j in range(len(mat[0])-1):
+            if mat[i][j] == mat[i+1][j] or mat[i][j+1] == mat[i][j]:
+                return 'not over'
+
+    for i in range(len(mat)):
+        for j in range(len(mat[0])):
+
+            if mat[i][j] == 0:
+                return 'not over'
+
+    for k in range(len(mat)-1):
+        if mat[len(mat)-1][k] == mat[len(mat)-1][k+1]:
+
+            return 'not over'
+
+    for j in range(len(mat)-1):
+        if mat[j][len(mat)-1] == mat[j+1][len(mat)-1]:
+
+            return 'not over'
+
+    return 'lose'
+
+def reverse(mat):
+    new = []
+    for i in range(len(mat)):
+        new.append([])
+        for j in range(len(mat[0])):
+            new[i].append(mat[i][len(mat[0])-j-1])
+    return new
+
+def transpose(mat):
+
+    new = []
+
+    for i in range(len(mat[0])):
+        new.append([])
+        for j in range(len(mat)):
+            new[i].append(mat[j][i])
+    return new
+
+def cover_up(mat):
+
+    new = []
+
+    for j in range(c.GRID_LEN):
+        partial_new = []
+        for i in range(c.GRID_LEN):
+            partial_new.append(0)
+        new.append(partial_new)
+    done = False
+    for i in range(c.GRID_LEN):
+        count = 0
+        for j in range(c.GRID_LEN):
+            if mat[i][j] != 0:
+                new[i][count] = mat[i][j]
+                if j != count:
+                    done = True
+                count += 1
+    return new, done
+
+
+def merge(mat):
+
+    done = False
+
+    for i in range(c.GRID_LEN):
+        for j in range(c.GRID_LEN-1):
+            if mat[i][j] == mat[i][j+1] and mat[i][j] != 0:
+                mat[i][j] *= 2
+                mat[i][j+1] = 0
+                done = True
+    return mat, done
+
+
+def up(game):
+
+    game = transpose(game)
+    game, done = cover_up(game)
+    temp = merge(game)
+    game = temp[0]
+    done = done or temp[1]
+    game = cover_up(game)[0]
+    game = transpose(game)
+    return game, done
+
+
+def down(game):
+
+    game = reverse(transpose(game))
+    game, done = cover_up(game)
+    temp = merge(game)
+    game = temp[0]
+    done = done or temp[1]
+    game = cover_up(game)[0]
+    game = transpose(reverse(game))
+    return game, done
+
+
+def left(game):
+
+    game, done = cover_up(game)
+    temp = merge(game)
+    game = temp[0]
+    done = done or temp[1]
+    game = cover_up(game)[0]
+    return game, done
+
+
+def right(game):
+
+    game = reverse(game)
+    game, done = cover_up(game)
+    temp = merge(game)
+    game = temp[0]
+    done = done or temp[1]
+    game = cover_up(game)[0]
+    game = reverse(game)
+    return game, done
